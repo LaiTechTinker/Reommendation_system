8ff50a6a842afc6dc59f6d4ae49db5c7813fad8e
laiwola <layiibrahim32@gmail.com>
2025-11-05 00:46:54 +0100

    data_validation_meth

--- Recommendation_system/pipline/training_pipeline.py
+++ Recommendation_system/pipline/training_pipeline.py
@@ -2,16 +2,16 @@ import sys
 from Recommendation_system.exception import RecomException
 from Recommendation_system.logger import logging
 from Recommendation_system.components.Data_ingestion import DataIngestion
-from Recommendation_system.entity.config_entity import DataIngestionConfig
-                             
-
-from Recommendation_system.entity.artifact_entity import DataIngestionArtifact
-                                            
+from Recommendation_system.components.Data_validation import DataValidation
+from Recommendation_system.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from Recommendation_system.entity.artifact_entity import (DataIngestionArtifact,DataValidationArtifact)  
+                                         
 
 
 class TrainPipeline:
     def __init__(self):
-        self.data_ingestion_config = DataIngestionConfig() 
+        self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config=DataValidationConfig() 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
         This method of TrainPipeline class is responsible for starting data ingestion component
@@ -28,6 +28,13 @@ class TrainPipeline:
             return data_ingestion_artifact
         except Exception as e:
             raise RecomException(e, sys) from e
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+        try:
+            logging.info("entered the start_data_validation method")
+            datavalidation=DataValidation(data_validation_config=self.data_validation_config,data_ingestion_artifact=data_ingestion_artifact)
+        except Exception as e:
+            raise RecomException(e,sys)
+
         
     def run_pipeline(self, ) -> None:
         """
@@ -35,7 +42,9 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             logging.info(f"data ingest artifact:{data_ingestion_artifact}")
+            logging.info(f"data ingest artifact:{data_validation_artifact}")
         except Exception as e:
             raise RecomException(e, sys)
         
