e002364dbfb7c0e953dfef72e4dcf30d67b82f1f
laiwola <layiibrahim32@gmail.com>
2025-12-20 23:41:05 +0100

    changes made to brach

--- Recommendation_system/components/embeddingpusher.py
+++ Recommendation_system/components/embeddingpusher.py
@@ -1,14 +1,14 @@
 import sys
 
 from Recommendation_system.cloudstorage.awsstorage import SimpleStorageService
-from Recommendation_system.exception import USvisaException
+from Recommendation_system.exception import RecomException
 from Recommendation_system.logger import logging
 from Recommendation_system.entity.artifact_entity import ModelPusherArtifact
 from Recommendation_system.entity.config_entity import ModelPusherConfig
 # from Recommendation_system.entity.s3_estimator import USvisaEstimator
 
 
-class ModelPusher:
+class EmbeddingPusher:
     def __init__(self, 
                  model_pusher_config: ModelPusherConfig):
         """
@@ -23,19 +23,14 @@ class ModelPusher:
 
     def initiate_model_pusher(self) -> ModelPusherArtifact:
         """
-        Method Name :   initiate_model_evaluation
-        Description :   This function is used to initiate all steps of the model pusher
-        
-        Output      :   Returns model evaluation artifact
-        On Failure  :   Write an exception log and then raise an exception
+     this function block will initiate the embedded vector to amazon s3 bucket
         """
-        logging.info("Entered initiate_model_pusher method of ModelTrainer class")
+        logging.info("Entered initiate_model_pusher method of  embdedding class")
 
         try:
             logging.info("Uploading artifacts folder to s3 bucket")
 
-            # self.usvisa_estimator.save_model(from_file=self.model_evaluation_artifact.trained_model_path)
-
+         
 
             model_pusher_artifact = ModelPusherArtifact(bucket_name=self.model_pusher_config.bucket_name,
                                                         s3_model_path=self.model_pusher_config.s3_model_key_path)
@@ -46,4 +41,4 @@ class ModelPusher:
             
             return model_pusher_artifact
         except Exception as e:
-            raise USvisaException(e, sys) from e
+            raise RecomException(e, sys) from e
